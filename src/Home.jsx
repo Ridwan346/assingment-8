122048877845dd69ee4bb1b84df55723bca3378a
Ridwan346 <ridwanhossain955@gmail.com>
2025-12-31 21:10:55 +0600

    fourth commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Nav from './nav and banner/Nav';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 import Footer from './nav and banner/Footer';
 
 const Home = () => {
+    let navigate = useNavigate()
     return (
        <div className='max-w-7xl m-auto'>
         <Nav></Nav>
+         {/* 🔄 Loading indicator */}
+            {navigation.state === "loading" && (
+                <div className="text-center py-6">
+                    <span className="loading loading-spinner loading-lg"></span>
+                </div>
+            )}
         <Outlet></Outlet>
         <Footer></Footer>
        </div>
